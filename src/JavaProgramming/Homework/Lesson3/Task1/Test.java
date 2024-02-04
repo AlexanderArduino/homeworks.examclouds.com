aa1a83a98b18996ff97ce953699db2ed99b0024e
Александр <140169354+AlexanderArduino@users.noreply.github.com>
2024-02-04 17:20:46 +0300

    Created

---
+++ src/JavaProgramming/Homework/Lesson3/Task1/Test.java
@@ -0,0 +1,12 @@
+package JavaProgramming.Homework.Lesson3.Task1;
+
+public class Test {
+// Однострочный комментарий
+/*
+* Многострочный комментарий
+*/
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
