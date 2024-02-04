16e8d43d5ebd591122205d4cd1f5c56dbc3a8e2b
Александр <140169354+AlexanderArduino@users.noreply.github.com>
2024-02-04 20:38:22 +0300

    Lesson3 Task6 end

--- src/JavaProgramming/Homework/Lesson3/Task6/Summ.java
+++ src/JavaProgramming/Homework/Lesson3/Task6/Summ.java
@@ -1,18 +1,24 @@
 package JavaProgramming.Homework.Lesson3.Task6;
 
-
 import java.util.Scanner;
 
 import static JavaProgramming.Homework.Lesson3.Task3.Calc.*;
 
+/**
+ * Создать программу, которая будет вычислять и выводить на экран сумму двух целых чисел,
+ * введённых пользователем с помощью класса Scanner.
+ * Если пользователь некорректно введёт хотя бы одно из чисел, то сообщать об ошибке.
+ */
 public class Summ {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int a = summa(sc.nextInt(), sc.nextInt());
+        if (sc.hasNextInt()) {
+            int a = summa(sc.nextInt(), sc.nextInt());
+            System.out.println(a);
+        } else {
+            System.out.println("Вы ввели некоректное число");
+        }
         sc.close();
-        System.out.println(a);
     }
-
 }
