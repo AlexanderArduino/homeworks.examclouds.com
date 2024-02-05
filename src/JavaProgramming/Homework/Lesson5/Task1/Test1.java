71e0bec726c16895499d90c2ccd689eaba691384
Александр <140169354+AlexanderArduino@users.noreply.github.com>
2024-02-05 21:58:01 +0300

    Lesson5 Task1 complite

--- src/JavaProgramming/Homework/Lesson5/Task1/Test1.java
+++ src/JavaProgramming/Homework/Lesson5/Task1/Test1.java
@@ -1,5 +1,13 @@
 package JavaProgramming.Homework.Lesson5.Task1;
 
+/**
+ *
+ * 1. Четное-нечетное.
+ * Передать на вход программы число в качестве аргумента.
+ * Если оно нечетное, распечатать его.
+ * Используем оператор if.
+ * Используйте метод Integer.parseInt() для преобразования из String в int.
+ */
 public class Test1 {
 
     public static String whatAreTheNumber(String s){
