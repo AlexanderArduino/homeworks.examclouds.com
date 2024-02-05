250de6f8be017c5ccd600b2c44b8a9258e6adf77
Александр <140169354+AlexanderArduino@users.noreply.github.com>
2024-02-05 11:03:14 +0300

    Lesson4 Test3 complite

--- src/JavaProgramming/Homework/Lesson4/Task2/Test2.java
+++ src/JavaProgramming/Homework/Lesson4/Task2/Test2.java
@@ -1,8 +1,18 @@
 package JavaProgramming.Homework.Lesson4.Task2;
 
+import java.io.Console;
+import java.util.Scanner;
+
+
+/**
+ * Меньшее по модулю число
+ * Создать программу, которая будет выводить на экран меньшее по модулю из трёх введённых пользователем вещественных чисел.
+ * Для вычисления модуля используйте тернарную условную операцию.
+ * Для ввода чисел используем класс Scanner.
+ */
 public class Test2 {
 
-    public static double minimalNumber(double a, double b, double c){
+    public static double minimalNumber(double a, double b, double c) {
 
         double result = Math.min(Math.abs(a), Math.abs(b));
         result = Math.min(result, Math.abs(c));
@@ -11,7 +21,14 @@ public class Test2 {
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = Math.abs(sc.nextDouble());
+        double b = Math.abs(sc.nextDouble());
+        double c = Math.abs(sc.nextDouble());
 
-        System.out.println(minimalNumber(0.000000000000001 , -4.3, -1.2));
+
+        System.out.println("Минимальное введенное число: " + minimalNumber(a, b, c));
+
+
     }
 }
---
+++ src/JavaProgramming/Homework/Lesson4/Task3/Girlyanda.java
@@ -0,0 +1,140 @@
+package JavaProgramming.Homework.Lesson4.Task3;
+
+
+/**
+ * Гирлянда, часть 1
+ * Имеется гирлянда, состоящая из 32 лампочек. Каждая лампочка имеет два состояния - включена или выключена.
+ * В начале работы программы случайным образом задайте какие лампочки будут включены. Напишите следующие методы:
+ * <p>
+ * метод blink, который будет мигать лампочками гирлянды один раз (операция ~);
+ * метод run, который будет запускать гирлянду в режим бегущей строки (операция << или >>);
+ * метод isFirstLampOn, который будет выяснять включена ли лампочка в первой позиции (наложение маски с помощью &);
+ * метод, который будет распечатывать текущее состояние гирлянды. Для получения двоичного формата числа используйте метод Integer.toBinaryString(a).
+ * Используйте побитовые операции.
+ */
+public class Girlyanda {
+
+    /**
+     * Инвертирует все 32 знака числа один раз
+     *
+     * @param lamp - число для инвертирования
+     * @return - возвращает после инвертирования
+     */
+    public static int blink(int lamp) {
+        return ~lamp;
+    }
+
+
+    /**
+     * Инвертирует все 32 знака числа n-раз
+     *
+     * @param lamp  - число для инвертирования
+     * @param count - количество n-раз для инвертирования
+     * @return - возвращает после инвертирования
+     */
+    public static int blink(int lamp, int count) {
+        for (int i = 0; i < count; i++) {
+            lamp = ~lamp;
+        }
+        return lamp;
+    }
+
+    /**
+     * Сдвигает число налево 1 раз
+     * @param lamp  - число для сдвига
+     * @return - возвращает после сдвига
+     */
+    public static int runLeft(int lamp) {
+        return runRight(lamp, 1);
+    }
+
+    /**
+     * Сдвигает число направо 1 раз
+     * @param lamp  - число для сдвига
+     * @return - возвращает после сдвига
+     */
+    public static int runRight(int lamp) {
+        return runLeft(lamp, 1);
+    }
+
+    /**
+     * Сдвигает число налево n-раз. Младшие биты заполняются нулями.
+     * @param lamp  - число для сдвига
+     * @param count - количество n-раз для сдвига
+     * @return - возвращает после сдвига
+     */
+    public static int runLeft(int lamp, int count) {
+        return lamp << count;
+    }
+
+    /**
+     * Сдвигает число направо n-раз. Старшие биты заполняются нулями.
+     * @param lamp  - число для сдвига
+     * @param count - количество n-раз для сдвига
+     * @return - возвращает после сдвига
+     */
+    public static int runRight(int lamp, int count) {
+        return lamp >>> count;
+    }
+
+    /**
+     * Выясняет, является ли первый бит числа единицей. Через функции runLeft() и runRight();
+     * @param lamp  - число для проверки
+     * @return   true - является, false - не является
+     */
+    public static boolean isFirstLampOn(int lamp) {
+        lamp = runLeft(lamp, 31);
+        lamp = runRight(lamp, 31);
+        if (lamp == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Выясняет, является ли первый бит числа единицей. Через операцию "&"
+     * @param lamp  - число для проверки
+     * @return   true - является, false - не является
+     */
+    public static boolean isFirstLampOn2(int lamp) {
+        lamp = lamp & 1;
+        if (lamp == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+
+class Test {
+    public static void main(String[] args) {
+        int lamp = 0b0000_0000_0000_0000_0000_0000_0000_0011;
+        int count = 31;
+        System.out.println(Girlyanda.isFirstLampOn2(lamp));
+
+/*
+        System.out.println(Integer.toBinaryString(lamp));
+        lamp = Girlyanda.runLeft(lamp,31);
+        System.out.println(Integer.toBinaryString(lamp));
+        lamp = Girlyanda.runRight(lamp,31);
+        System.out.println(Integer.toBinaryString(lamp));
+*/
+
+
+        /*
+        System.out.println(Integer.toBinaryString(Girlyanda.blink(lamp, count)));
+        System.out.println(Integer.toBinaryString(Girlyanda.blink(lamp)));
+        System.out.println("");
+        System.out.println(Integer.toBinaryString(Girlyanda.runLeft(lamp)));
+        System.out.println(Integer.toBinaryString(Girlyanda.runLeft(lamp,count)));
+        System.out.println("");
+        System.out.println(Integer.toBinaryString(Girlyanda.runRight(lamp)));
+        System.out.println(Integer.toBinaryString(Girlyanda.runRight(lamp, count)));
+        */
+
+
+    }
+
+}
