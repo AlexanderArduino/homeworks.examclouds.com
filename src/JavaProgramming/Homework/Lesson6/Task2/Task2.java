3b230aafb001b05cdb47140a4694e891a411673c
Alex <a.i.anohin.arduino@gmail.com>
2024-02-08 11:21:30 +0300

    Lesson6 Task2 complite

---
+++ src/JavaProgramming/Homework/Lesson6/Task2/Task2.java
@@ -0,0 +1,19 @@
+package JavaProgramming.Homework.Lesson6.Task2;
+
+
+/**
+ * 2. Одномерный массив типа double.
+ * Создать массив типа double с размером 4.
+ * Записать в него любые значения с помощью блока для инициализации.
+ * Вывести на консоль значение первого элемента.
+ */
+public class Task2 {
+    static double[] db = {0.5 , 0.6 , 0.8 , 1.2};
+
+
+    public static void main(String[] args) {
+        System.out.println(db[0]);
+    }
+}
+
+
