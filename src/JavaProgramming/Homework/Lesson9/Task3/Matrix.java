f506083be02a632a2515607b51b9b1dedac0dd21
Александр <140169354+AlexanderArduino@users.noreply.github.com>
2024-02-14 23:04:55 +0300

    Lesson9 Task UPD (not complite)

--- src/JavaProgramming/Homework/Lesson9/Task3/Matrix.java
+++ src/JavaProgramming/Homework/Lesson9/Task3/Matrix.java
@@ -14,78 +14,116 @@ import java.util.Arrays;
  * умножение матриц.
  */
 public class Matrix {
-    private int kolichestvoStolbcov;
-    private int kolichestvoStrok;
-    private double [][] matrix;
+    private int kolichestvoStolbcov;        //Количество столбцов матрицы
+    private int kolichestvoStrok;           //Количество строк матрицы
+    private double[][] matrix;              // Сама матрица
 
-    Matrix(int kolichestvoStrok, int kolichestvoStolbcov){
+    /**
+     * Конструктор создания двухмерной матрицы с заданным количеством строк и столбцов
+     *
+     * @param kolichestvoStrok    - количество строк
+     * @param kolichestvoStolbcov - количство столбцов
+     */
+    Matrix(int kolichestvoStrok, int kolichestvoStolbcov) {
         this.matrix = new double[kolichestvoStrok][kolichestvoStolbcov];
     }
 
-    public int getKolichestvoStolbcov(){
+    public int getKolichestvoStolbcov() {
         return kolichestvoStolbcov;
     }
-    public int getKolichestvoStrok(){
+
+    public int getKolichestvoStrok() {
         return kolichestvoStrok;
     }
-    public double [][] getMatrix(){
+
+    public double[][] getMatrix() {
         return matrix;
     }
 
-    public void setKolichestvoStolbcov(int kolichestvoStolbcov){
+    public void setKolichestvoStolbcov(int kolichestvoStolbcov) {
         this.kolichestvoStolbcov = kolichestvoStolbcov;
     }
-    public void setKolichestvoStrok(int kolichestvoStrok){
+
+    public void setKolichestvoStrok(int kolichestvoStrok) {
         this.kolichestvoStrok = kolichestvoStrok;
     }
-    public void setMatrix(double [][] matrix){
+
+    public void setMatrix(double[][] matrix) {
         this.matrix = matrix;
     }
 
     /**
      * Заполнение матрицы рандомными целыми положительными числами
      */
-    private double [][] fillMatrixRandomPozitiveIntegerNumber(){
+    private double[][] fillMatrixRandomPozitiveIntegerNumber() {
         for (int i = 0; i < this.matrix.length; i++) {
             for (int j = 0; j < this.matrix[i].length; j++) {
-                this.matrix[i][j] = Math.round(Math.random()*Math.random()*100);
+                this.matrix[i][j] = Math.round(Math.random() * Math.random() * 100);
             }
         }
         return matrix;
     }
-    public void fillMatrixRandomIntegerNumber(){
+
+    /**
+     * Заполнение матрицы целыми случайными положительными и отрицательными числами
+     */
+    public void fillMatrixRandomIntegerNumber() {
         this.matrix = fillMatrixRandomPozitiveIntegerNumber();
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                if(Math.random() < 0.5){
+                if (Math.random() < 0.5) {
                     matrix[i][j] *= -1;
                 }
             }
         }
     }
 
-    private void fillMatrixRandomPozitiveDoubleNumber(int number){
+    /**
+     * Заполнение матрицы рандомными вещественными положительными числами
+     */
+    private double[][] fillMatrixRandomPozitiveDoubleNumber(int number) {
         for (int i = 0; i < this.matrix.length; i++) {
             for (int j = 0; j < this.matrix[i].length; j++) {
-                double value = (Math.random()*Math.random()*100);
+                double value = (Math.random() * Math.random() * 100);
                 double scale = Math.pow(10, number);
-                this.matrix[i][j] = Math.ceil(value*scale)/scale;
+                this.matrix[i][j] = Math.ceil(value * scale) / scale;
+            }
+        }
+        return matrix;
+    }
+
+    /**
+     * Заполнение матрицы вещественными случайными положительными и отрицательными числами
+     *
+     * @param number - количество знаков после запятой
+     */
+    public void fillMatrixRandomDoubleNumber(int number) {
+        this.matrix = fillMatrixRandomPozitiveDoubleNumber(number);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (Math.random() < 0.5) {
+                    matrix[i][j] *= -1;
+                }
             }
         }
     }
 
-    public void printMatrix(){
+    /**
+     *
+     */
+    public void printMatrix() {
         for (int i = 0; i < matrix.length; i++) {
             System.out.println(Arrays.toString(matrix[i]));
         }
     }
+
 }
 
 
-class Test{
+class Test {
     public static void main(String[] args) {
-        Matrix mx1 = new Matrix(3,4);
-        Matrix mx2 = new Matrix(3,4);
+        Matrix mx1 = new Matrix(3, 4);
+        Matrix mx2 = new Matrix(3, 4);
 
         mx1.fillMatrixRandomIntegerNumber();
         System.out.println(" ");
@@ -96,8 +134,8 @@ class Test{
 //        mx2.printMatrix();
 
 
-    double value = Math.random()*Math.random()*100;
-    double scale = Math.pow(10,3);
+        double value = Math.random() * Math.random() * 100;
+        double scale = Math.pow(10, 3);
 
 
     }
