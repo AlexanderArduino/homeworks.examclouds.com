28685baa578c8f8a9828380cf54507d42f5b13c0
Alex <a.i.anohin.arduino@gmail.com>
2024-03-05 15:53:23 +0300

    Lesson9 Task5 не завершена. Нужно еще подумать, как сдлелать задание через рекурсию

---
+++ src/JavaProgramming/Homework/Lesson9/Task5/Test5.java
@@ -0,0 +1,17 @@
+package JavaProgramming.Homework.Lesson9.Task5;
+
+/*
+* Даны два целых числа A и В. Выведите все числа от A до B включительно,
+* в порядке возрастания, если A < B, или в порядке убывания в противном случае.
+* Использовать рекурсию.
+* */
+public class Test5 {
+
+    public void printNumber(int a, int b){
+        if(a == b) {
+            System.out.println(a);
+        } else if(a < b){
+            // !!!!!!!! ПОКА НЕ ПОЙМУ КАК СДЕЛАТЬ ЭТУ ЗАДАЧУ !!!!!!
+        }
+    }
+}
