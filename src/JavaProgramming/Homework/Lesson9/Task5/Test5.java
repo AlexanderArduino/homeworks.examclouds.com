5b62151e37c8e69aa3e730e316c873f423373c16
Александр <140169354+AlexanderArduino@users.noreply.github.com>
2024-03-15 20:55:45 +0300

    UPD

--- src/JavaProgramming/Homework/Lesson9/Task5/Test5.java
+++ src/JavaProgramming/Homework/Lesson9/Task5/Test5.java
@@ -4,14 +4,30 @@ package JavaProgramming.Homework.Lesson9.Task5;
 * Даны два целых числа A и В. Выведите все числа от A до B включительно,
 * в порядке возрастания, если A < B, или в порядке убывания в противном случае.
 * Использовать рекурсию.
+*
+* СДЕЛАНО КОРЯВО, ВЫСКАКИВАЕТ ИСКЛЮЧЕНИЕ StackOverflow
 * */
 public class Test5 {
 
-    public void printNumber(int a, int b){
+    public static void printNumber(int a, int b){
         if(a == b) {
             System.out.println(a);
         } else if(a < b){
-            // !!!!!!!! ПОКА НЕ ПОЙМУ КАК СДЕЛАТЬ ЭТУ ЗАДАЧУ !!!!!!
+            System.out.print(a + " ");
+            a++;
+            printNumber(a,b);
+        } else if(a>b){
+            System.out.print(a + " ");
+            a--;
+            printNumber(a,b);
         }
     }
+
+    public static void main(String[] args) {
+        printNumber(1,1);
+        printNumber(1,500);
+        printNumber(500,1);
+    }
 }
+
+
